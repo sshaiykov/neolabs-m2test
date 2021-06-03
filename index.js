c7b507fe5b25c8e01ec358043866aed4b84b0391
Samat <Shaiykov7@mail.ru>
2021-06-03 19:57:16 +0500

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const bot = require('./utils/bootstrap')
-const { startWF, chooseDay } = require('./utils/keyboard')
+const { startWF, chooseDay, daysOfWeek } = require('./utils/keyboard')
 const { chooseWeather, days, findDay} = require('./utils/weather')
 
 bot.start(ctx => ctx.reply(`Привет, ${ctx.message.from.first_name}!`, startWF()))
 
 
 bot.hears('погода', ctx => ctx.reply(`Сегодня ${chooseWeather()}`))
- 
-bot.hears(days, ctx => ctx.reply(findDay()))
 
-bot.hears('Выбрать день', ctx => ctx.reply('Выберите день:', chooseDay()))
+bot.hears('Выбрать день', ctx => ctx.reply('Выберите день:', daysOfWeek()))
+
+bot.hears(days, ctx => ctx.reply(chooseDay(ctx.message.text)))
 
 bot.hears('Погода на сегодня', ctx => ctx.reply(chooseWeather()))
 
